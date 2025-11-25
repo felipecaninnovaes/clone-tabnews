14f94b56737e267ef8acf76848105d38d9d425da
Felipe Canin Novaes <felipe@felipecncloud.com>
2025-11-24 22:01:39 -0300

    adiconado scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Aprendento o projeto.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Isso era para ter seu CEP mas lembrei que não existe. 😊</h1>
+  return <h1>Isso era para ter seu CEP mas lembrei que não existe. 😊</h1>;
 }
 
-export default Home
+export default Home;
