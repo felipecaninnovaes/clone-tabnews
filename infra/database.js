05a177ba8d358883a4f5f6a16cfcab88bc3c986f
Felipe Canin Novaes <felipe@felipecncloud.com>
2026-02-07 12:23:17 -0300

    Disable SSL no Banco de Dados.

--- infra/database.js
+++ infra/database.js
@@ -27,7 +27,7 @@ function getSSLValues() {
       ca: process.env.POSTGRES_CA,
     };
   }
-  return process.env.NODE_ENV == 'development' ? false : true
+  return process.env.NODE_ENV == 'development' ? false : false
 }
 
 export default {
