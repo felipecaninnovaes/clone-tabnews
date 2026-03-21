9361783209d68804962d8e8392283ac1593e0588
Felipe Canin Novaes <felipe@felipecncloud.com>
2026-03-21 14:29:31 -0300

    Adiconado test e rodado o POST sem o dryRun.

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,17 +1,30 @@
-import migrationRunner from 'node-pg-migrate';
-import {join} from 'node:path';
-
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
 
 export default async function migration(request, response) {
-  const migrations = await migrationRunner({
-    databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
-    dir: join(process.cwd(), 'infra', 'migrations'),
-    direction: 'up',
-    verbose: true,
-    migrationsTable: 'pgmigrations',
-  });
-  response.status(200).json(migrations);
+  if (request.method === "POST") {
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: false,
+      dir: join(process.cwd(), "infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+    response.status(200).json(migrations);
+  }
+
+  if (request.method === "GET") {
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: true,
+      dir: join(process.cwd(), "infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+    response.status(200).json(migrations);
+  }
+
+  response.status(405).json({ error: "Method not allowed" });
 }
-
-
