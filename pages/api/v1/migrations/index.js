478fbc02229c888e60c807958293efb3d1cb4e2a
Felipe Canin Novaes <felipe@felipecncloud.com>
2026-03-21 13:35:59 -0300

    refactor: update migration directory and enhance migration runner configuration
    - Changed migration directory from 'infra/migration' to 'infra/migrations' in package.json scripts.
    - Removed unused 'node-pg-migrations' dependency from package.json.
    - Updated migration runner in 'pages/api/v1/migrations/index.js' to use 'node-pg-migrate' with improved configuration options including database URL, dry run, and verbose logging.

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,8 +1,17 @@
-import migrationRunner from 'node-pg-migrations';
+import migrationRunner from 'node-pg-migrate';
+import {join} from 'node:path';
+
 
 export default async function migration(request, response) {
-  // const migrations = await migrationRunner.run({});
-  response.status(200).json([]);
+  const migrations = await migrationRunner({
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join(process.cwd(), 'infra', 'migrations'),
+    direction: 'up',
+    verbose: true,
+    migrationsTable: 'pgmigrations',
+  });
+  response.status(200).json(migrations);
 }
 
 
