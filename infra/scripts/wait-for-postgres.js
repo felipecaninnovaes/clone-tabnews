9bf18ff7874a88f8d00ba7b204cb8e5f96f9d67e
Felipe Canin Novaes <felipe@felipecncloud.com>
2026-03-22 15:19:21 -0300

    feat: adicionar script para aguardar conexão do PostgreSQL e atualizar comandos no package.json

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,21 @@
+const { exec } = require("node:child_process");
+
+function checkPostgresConnection() {
+  exec(
+    "docker exec postgres-development pg_isready --host localhost",
+    handleReturn,
+  );
+
+  function handleReturn(error, stdout, stderr) {
+    if (stdout.search("accepting connections") === -1) {
+      console.log("🟡 Não está aceitando conexões, tentando novamente...");
+      setTimeout(checkPostgresConnection, 1000);
+      return;
+    }
+    console.log("🟢 PostgreSQL está aceitando conexões!");
+  }
+}
+
+console.log("🔴 Aguardando o PostgreSQL aceitar conexões...");
+
+checkPostgresConnection();
